8b1bc6e19349109c0d39852b7a66c9940acdd484
skrokszczepan <skrokskrok@gmail.com>
2026-01-09 16:48:04 +0100

    Add files via upload

---
+++ Zadanie 1.py
@@ -0,0 +1,14 @@
+# Najpierw utwórz zmienną o nazwie x i przypisz jej wartość 10
+x = 10
+print("Pierwszy x to ", x)
+# Następnie utwórz drugą zmienną y i przypisz jej wartość 55
+y = 55
+print("y to ", y)
+# Utwórz  zmienną z i przypisz jej wartość x + y
+z = x + y
+print("z to ", z)
+# Zastąp x wartością z
+x = z
+
+#Wydrukuj wartość x za pomocą funkcji Pythona print()
+print("A x po zmianie to ", x)
