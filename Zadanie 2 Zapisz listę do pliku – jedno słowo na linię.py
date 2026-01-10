18117da43a8c279f30b9ff2cb159d51fda22f0ef
skrokszczepan <skrokskrok@gmail.com>
2026-01-10 09:25:24 +0100

    Update Zadanie 2 Zapisz listę do pliku – jedno słowo na linię.py

--- Zadanie 2 Zapisz listę do pliku – jedno słowo na linię.py
+++ Zadanie 2 Zapisz listę do pliku – jedno słowo na linię.py
@@ -4,7 +4,9 @@ with open("fruits.txt", "w") as file:
     for word in words:
         file.write(word + "\n")
 
+#Sprawdzenie czy sie udało
 with open("fruits.txt", "r") as file:
     lista = file.read()
 
 print("W pliku znajduje się lista: \n" + lista)
+
