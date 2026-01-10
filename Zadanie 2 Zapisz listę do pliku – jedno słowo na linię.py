13f8956d4c864d0b331566aeec0a709b8f900219
skrokszczepan <skrokskrok@gmail.com>
2026-01-10 09:24:28 +0100

    Add files via upload

---
+++ Zadanie 2 Zapisz listę do pliku – jedno słowo na linię.py
@@ -0,0 +1,10 @@
+words = ["jabłko", "banan", "wiśnia"]
+
+with open("fruits.txt", "w") as file:
+    for word in words:
+        file.write(word + "\n")
+
+with open("fruits.txt", "r") as file:
+    lista = file.read()
+
+print("W pliku znajduje się lista: \n" + lista)
